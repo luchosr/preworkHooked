cdfb2e29f209ffcc38e1c7841419de5d3657796b
Luciano <luchosr@gmail.com>
2019-07-01 16:26:44 -0300

    add new folder structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
 import "./App.css";
-import Sidebar from "./Sidebar/Sidebar";
-import MainContainer from "./Main/Container/MainContainer";
+import Sidebar from "./components/Sidebar/Sidebar";
+import MainContainer from "./components/Main/Container/MainContainer";
+import JSReddit from "./components/Main/JSReddit/JSReddit";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock, faWrench } from "@fortawesome/free-solid-svg-icons";
+import {
+  faRedditAlien,
+  faGithub,
+  faReddit
+} from "@fortawesome/free-brands-svg-icons";
+
+library.add(faRedditAlien, faClock, faGithub, faWrench, faReddit);
 
 function App() {
   return (
@@ -13,7 +24,7 @@ function App() {
         width="25%"
         title="Popular on r/javascript"
         itemComponent="JSReddit"
-        titleIcon="faRedditAlien"
+        titleIcon="wrench"
       />
     </div>
   );
--- src/components/Main/Container/MainContainer.js
+++ src/components/Main/Container/MainContainer.js
@@ -2,26 +2,30 @@ import React, { useState, useEffect } from "react";
 import "./styles.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faRedditAlien, faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faClock } from "@fortawesome/free-solid-svg-icons";
-import JSReddit from "../JSReddit/JSReddit";
-library.add(faRedditAlien, faClock, faGithub);
+import {
+  faRedditAlien,
+  faGithub,
+  faReddit
+} from "@fortawesome/free-brands-svg-icons";
+import { faClock, faWrench } from "@fortawesome/free-solid-svg-icons";
+import JSReddit from "../JSReddit/JSReddit.js";
+library.add(faRedditAlien, faClock, faGithub, faWrench, faReddit);
 
 const MainContainer = props => {
-  let listItems = props.items;
+  const { itemComponent: ItemComponent } = props;
   return (
     <div
       className="container"
       style={{ maxHeight: props.maxHeight, width: props.width }}
     >
       <h4 className="container__title">
-        <span>
+        <span className="container__icon">
           <FontAwesomeIcon icon={props.titleIcon} />
         </span>
         {props.title}
       </h4>
       <ul className="container__uList">
-        <props.itemComponent />
+        <ItemComponent />
       </ul>
     </div>
   );
