3513f9485aa6bfcce11350cdb3221af5d7b113ed
Luciano <luchosr@gmail.com>
2019-07-02 13:36:13 -0300

    add RedditPost import

--- src/components/Main/JSReddit/JSReddit.js
+++ src/components/Main/JSReddit/JSReddit.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import RedditPost from "../RedditPost/RedditPost"
 
 const JSReddit = () => {
   const [error, seterror] = useState(null);
@@ -22,30 +23,9 @@ const JSReddit = () => {
   };
   return (
     // aca mapeas reddit post y retornas la plantilla con los datos.
-    <li className="redditNews">
-      <h5
-        className="postTittle"
-        style={{ color: "#F8F8F2", margin: "0 0 4px 0", fontSize: "14px" }}
-      >
-        titulo asd asd as asd
-      </h5>
-      <h6
-        className="postAuthor"
-        style={{ color: "#66d9ef", margin: "0 0 4px 0", fontSize: "14px" }}
-      >
-        Posted By <span style={{ color: "#f1fa8c" }}>autor</span>{" "}
-      </h6>
-      <h6 className="score" style={{ color: "#6272a4", margin: "0 0 4px 0" }}>
-        reddit score puntos
-      </h6>
-      <a
-        href="www.google.com"
-        className="comments"
-        style={{ textDecoration: "none", color: "#ff79c6", padding: "10px" }}
-      >
-        Link to Comments
-      </a>
-    </li>
+{
+  redditPost.map(post => )
+}
   );
 };
 
